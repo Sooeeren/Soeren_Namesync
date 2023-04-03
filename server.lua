1c12a705f64a84bdadd53965a336e9ac6f93f097
Sooeeren <115920137+Sooeeren@users.noreply.github.com>
2023-04-03 15:07:53 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,7 +4,7 @@ RegisterNetEvent('esx:playerLoaded', function(player, xPlayer)
     for k,v in pairs(GetPlayerIdentifiers(player)) do
         if string.sub(v, 1, string.len("discord:")) == "discord:" then
             local discordid = string.sub(v, string.len("discord:") + 1)
-            exports['only_namesync']:UpdateNickname(xPlayer.name, discordid)
+            exports['Soeren_Namesync']:UpdateNickname(xPlayer.name, discordid)
         end
     end
 end)
